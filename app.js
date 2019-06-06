6fc952c697cf139268fda0644517f7616e30e358
Romeo <romeojeremiah@gmail.com>
2019-06-05 20:10:36 -0500

    change home route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/', function(req, res){
+app.get(__dirname + '/', function(req, res){
   res.render('home', {homeStartingContent: homeStartingContent, posts:posts});
 });
 
